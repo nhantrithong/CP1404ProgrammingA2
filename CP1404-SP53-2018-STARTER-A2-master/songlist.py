d3e28579b9ba9a9833f8c6502598cad5884510a5
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-01-06 15:58:52 +0800

    Added access to song.csv file and init method

---
+++ CP1404-SP53-2018-STARTER-A2-master/songlist.py
@@ -0,0 +1,24 @@
+input_file = open("songs.csv", "r")
+FILES = input_file.readlines()
+
+class SongList:
+    def __init__(self, list = []):
+        self.list = list
+        self.file = FILES
+
+        self.count_list = []
+        self.learnt_count = []
+
+        self.remainder = [1]
+
+        self.sort_list = []
+        self.sort_list_2 = []
+
+        self.sort_artist_list = []
+        self.sort_artist_list_2 = []
+
+        self.sort_year_list = []
+        self.sort_year_list_2 = []
+
+        self.learnt_list = []
+        self.not_learnt_list = []
