4764065650239f0160ff5c97895797522f39de1a
nhantrithong <trithong.nhan@my.jcu.edu.au>
2019-01-06 16:12:43 +0800

    Added create_widgets() method and song_display() method which depends on each other

--- CP1404-SP53-2018-STARTER-A2-master/main.py
+++ CP1404-SP53-2018-STARTER-A2-master/main.py
@@ -67,3 +67,44 @@ class SongsToLearnApp(App):
         elif self.root.ids.sort_selection.values == 'Year':
             choice = self.list_year_2
         return choice
+
+    def create_widgets(self):
+        self.root.ids.create_widgets.clear_widgets()
+        self.root.ids.output_label.text = "To Learn: {}, Learnt: {}".format(self.get_num_total - self.get_num_learnt,
+                                                                            self.get_num_learnt)
+        temp_button = Button
+        result = self.list
+        for songs in result:
+            song_here = Song()
+            comps = songs.split(",")
+            Title = comps[0]
+            Artist = comps[1]
+            Year = comps[2]
+            Status = comps[3]
+            # song_summary = self.song_display(Title, Artist, Year, Status)
+            song_summary = "{},{},{} {}".format(Title, Artist, Year, Status)
+            if Status == "*":
+                button_color = [1, 0, 0, 1]
+            elif Status == "":
+                button_color = [0, 0, 1, 1]
+            temp_button = Button(text=song_summary, background_color=button_color)
+            temp_button.bind(on_release=self.song_display)
+            self.root.ids.create_widgets.add_widget(temp_button)
+
+    def song_display(self, temp_button):
+        temp_button.background_color = [0, 0, 1, 1]
+        self.get_song = self.songlist.get_song
+        button_message = temp_button.text
+        new_message = button_message.split(",")
+        get_title = new_message[0]
+        full_song = self.get_song(get_title)
+        parts = full_song.split(",")
+        if parts[3] == "*":
+            marked = "(Learnt)"
+        elif parts[3] == "":
+            marked = "(Already Learnt)"
+        final = parts[0] + ',' + parts[1] + ',' + parts[2] + ' ' + marked
+        final_2 = parts[0] + ',' + parts[1] + ',' + parts[2] + ' ' + marked
+        temp_button.text = final
+        self.songlist.save_songs()
+        self.root.ids.output_label_2.text = final_2
